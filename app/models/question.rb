83e0c085f38b69162aaecd3986f5cabff3b2f38f
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-06-13 23:08:00 +0300

    19-23_14-06-2019

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,8 @@
 class Question < ApplicationRecord
+<<<<<<< HEAD
   has_many :answers, inverse_of: :question, dependent: :delete_all
 
+=======
+>>>>>>> 65baf0b... 23-07_13-06-2019
   validates :title, :body, presence: true
 end
