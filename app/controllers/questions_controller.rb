5d7e490784946550f36d931562b6d103cea25b4a
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-06-15 01:09:48 +0300

    01-09_15-06-2019 Adds `DELETE #destroy` into question_controller_spec

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit, :update]
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -23,6 +23,11 @@ class QuestionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   def update
