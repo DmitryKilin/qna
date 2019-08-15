12241f5f3234c17c748ff992b3c9c560462010de
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-08-15 17:42:26 +0300

    17-41_15-08-2019. Lesson-10

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates :definition, presence: true, inclusion: { in: [1, -1] }
+  validates :votable, uniqueness: { scope: :user }
 end
