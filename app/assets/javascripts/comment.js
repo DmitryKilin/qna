dff593c921ba763819d050a08c13a8a4b129a75d
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-09-13 16:12:25 +0300

    16-11_13-09-2019 Created JS to show Errors. Lesson 11

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,23 +1,35 @@
 $(document).on('turbolinks:load', function () {
         $('form.new-comment').on('ajax:success', function (e) {
             var xhr = e.detail[0];
-            var $commentsDiv = $('.' + xhr['commented_resource'] + ' .comments .comments-list');
+            var $divComments = $('.' + xhr['commented_resource'] + ' .comments .comments-list');
 
-            $commentsDiv.append(
+            $divComments.append(
                 '<div class="comment" id="comment-' + xhr['comment_id'] + '">' +
                     '<p>' + xhr['comment_body'] + '</p>' +
                     '<p class="small">' + xhr['user_email'] + '</p>' +
                 '</div>'
                 );
 
-            console.log(e)
-
-
-            // var resourceName = xhr['commentable_type'].toLowerCase();
-            // var resourceId = xhr['commentable_id'];
-            // var resourceContent = xhr['body'];
-
-            // $('.' + resourceName + '-' + resourceId + ' .comment-block .comments').append('<div class="comment"><p>' + resourceContent + '</p></div>');
         })
+            .on('ajax:error', function (e) {
+                var errors = e.detail[0].body;
+                var $divErrors = $('.notifications');
+                console.log(errors.length);
+
+                $divErrors.children().remove();
+                $divErrors.prepend(
+                    '<div class="alert alert-danger alert-dismissible fade show"' +
+                        '<stong>' + errors.length + ' error(s) detected: ' + '</stong>' +
+                        '<ul>' +
+                            $.each(errors, function(index, value) {
+                                return '<li>' + index + value + '</li>'
+                            }) +
+                        '</ul>' +
+                        '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                            '<span aria-hidden="true">' + '&times' + '</span>' +
+                        '</button>' +
+                    '</div>'
+                );
+            })
     }
 );
