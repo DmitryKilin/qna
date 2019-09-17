bb2432db6e5edc0292c6b58211fa164150fc8388
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-09-17 23:50:45 +0300

    23-50_17-09-2019. Specs passes.  Lesson-11

---
+++ spec/features/comment/comment_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+feature 'It can a comments be added. ' do
+  given(:user1) {create(:user)}
+  given(:user2) {create(:user)}
+  given(:question) {create(:question)}
+  given(:answer) {create(:answer)}
+
+
+  scenario 'Authorized user can comment question and all subscribes can see it.', js: true do
+    Capybara.using_session('user1') do
+      sign_in(user1)
+      visit question_path(question)
+    end
+
+    Capybara.using_session('user2') do
+      sign_in(user2)
+      visit question_path(question)
+    end
+
+    Capybara.using_session('user1') do
+      within '.question .new-comment' do
+        fill_in :comment_body, with: 'My comment'
+        click_on 'Comment'
+      end
+      within '.question .comments-list' do
+        expect(page).to have_content 'My comment'
+      end
+    end
+
+    Capybara.using_session('user2') do
+      within '.question .comments-list' do
+        expect(page).to have_content 'My comment'
+      end
+    end
+  end
+
+  scenario 'Authorized user can comment answer and all subscribes can see it.', js: true do
+    Capybara.using_session('user1') do
+      sign_in(user1)
+      visit question_path(answer.question)
+    end
+
+    Capybara.using_session('user2') do
+      sign_in(user2)
+      visit question_path(answer.question)
+    end
+
+    Capybara.using_session('user1') do
+      within '.answers .new-comment' do
+        fill_in :comment_body, with: 'My comment'
+        click_on 'Comment'
+      end
+      within '.answers .comments-list' do
+        expect(page).to have_content 'My comment'
+      end
+    end
+
+    Capybara.using_session('user2') do
+      within '.answers .comments-list' do
+        expect(page).to have_content 'My comment'
+      end
+    end
+  end
+
+  scenario 'It shows an error message when user tries add a blank comment. ', js: true do
+    sign_in(user1)
+    visit question_path(question)
+
+    within '.question .new-comment' do
+      fill_in :comment_body, with: ''
+      click_on 'Comment'
+    end
+
+    within '.notifications' do
+      expect(page).to have_content "Body can't be blank"
+    end
+  end
+
+end
