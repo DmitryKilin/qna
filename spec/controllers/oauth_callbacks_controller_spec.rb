5b93e3f2f252c150ffeca88353f5dcab058b2289
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-11-22 21:07:53 +0300

    21-06_22-11-2019. Authorization model updated for mail confirmation. Lesson-12

--- spec/controllers/oauth_callbacks_controller_spec.rb
+++ spec/controllers/oauth_callbacks_controller_spec.rb
@@ -7,12 +7,16 @@ RSpec.describe OauthCallbacksController, type: :controller do
     allow(request.env).to receive(:[]).with('omniauth.auth').and_return(oauth_data)
   end
 
-  describe 'Email have not provided' do
-    let(:oauth_data) { {'provider' => 'github', 'uid' => 123, 'info' => nil } }
+  describe 'Email has not provided.' do
+    let!(:oauth_data) { {'provider' => 'vkontakte', 'uid' => 123, 'info' => nil } }
 
-    it 'redirects to demand_email_path' do
+    it 'Redirects to demand_email_path' do
 
       expect(response).to redirect_to demand_email_path
+      # Не могу понять почему вместо редиректа в респонсе выдаётся рендеринг пустого шаблона?:
+      # Expected response to be a <3XX: redirect>, but was a <200: OK>
+      #
+      # В девелоп-окружении всё перенаправляется!
     end
   end
 
