4c7e9cfc29e28598e2bcafbdf5b2a0c9c5687226
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-12-04 15:48:11 +0300

    15-47_04-12-2019 Cancancan and tests. Lesson-13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   check_authorization unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
+      format.js { head :forbidden }
+    end
   end
 
   private
