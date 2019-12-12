bfe000cfa2bf548c18714ad51b6a22a7d654208c
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2019-12-12 23:19:16 +0300

    23-18_12-12-2019 API answer show implemented. Lesson-14

--- spec/api/v1/answers/answer_show_spec.rb
+++ spec/api/v1/answers/answer_show_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-describe 'AnswerShow' do
+describe 'Answer API #show', type: :request do
+  let(:headers) do
+    { 'CONTENT_TYPE' => 'application/json',
+      'ACCEPT' => 'application/json' }
+  end
 
+  describe 'GET /api/v1/answers/:id' do
+    let!(:answer) { create(:answer) }
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+    let(:access_token) { create(:access_token, resource_owner_id: answer.user.id) }
+    let(:resource_response) { json['answer'] }
+    let(:resource_essence) { answer }
+    let(:public_fields) { %w[id body created_at updated_at] }
+
+    it_behaves_like 'API Authorizable'
+    it_behaves_like 'API Attachable'
+    it_behaves_like 'API Linkable'
+    it_behaves_like 'API Commentable'
+    it_behaves_like 'API Public fields visible'
+  end
 end
