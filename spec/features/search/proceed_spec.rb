9b7b597ed4df329e6574f09eefddb6a7c4258463
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2020-01-09 00:16:43 +0300

    00-16_09-01-2020 Some tests added. Lesson-16

--- spec/features/search/proceed_spec.rb
+++ spec/features/search/proceed_spec.rb
@@ -1,19 +1,22 @@
-require 'rails_helper'
+require 'sphinx_helper'
 
-feature 'Пользователь может искать вопрос по строке символов. ' do
+feature 'Guests and users can search a phrase in Questions, Answers, Users and Comments  ' do
   given!(:question1) { create(:question, title: "The slings and arrows of outrageous fortune")}
   given!(:question2) { create(:question, title: 'Or to take arms against a sea of troubles')}
-  scenario 'A quest can find a question keeping a string in a questions title or a body. ', js: true do
-    visit search_get_search_path
+  before { visit search_get_search_path }
 
-    within 'form' do
-      fill_in 'Search:', with: 'to take arms'
-      click_on 'Search'
-    end
-
-    expect(current_path).to eq search_get_search_path
-    within '#question_entries' do
-      expect(page).to have_content 'to take arms'
+  describe 'Searching in ', js: true, sphinx: true do
+    scenario 'a questions' do
+      ThinkingSphinx::Test.run do
+        within 'form' do
+          fill_in 'Search:', with: 'to take arms'
+          click_on 'Search'
+        end
+        expect(current_path).to eq search_get_search_path
+        within '#question' do
+          expect(page).to have_content question2.id
+        end
+      end
     end
   end
 end
