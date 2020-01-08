e20a0298ca71aae0b67c53bd00a7eea1d10a22d1
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2020-01-09 00:43:46 +0300

    00-43_09-01-2020 The proceed_spec.rb passes. Lesson-16

--- spec/features/search/proceed_spec.rb
+++ spec/features/search/proceed_spec.rb
@@ -3,6 +3,12 @@ require 'sphinx_helper'
 feature 'Guests and users can search a phrase in Questions, Answers, Users and Comments  ' do
   given!(:question1) { create(:question, title: "The slings and arrows of outrageous fortune")}
   given!(:question2) { create(:question, title: 'Or to take arms against a sea of troubles')}
+  given!(:answer1) { create(:answer, body: "The slings and arrows of outrageous fortune")}
+  given!(:answer2) { create(:answer, body: 'Or to take arms against a sea of troubles')}
+  given!(:comment1) { create(:comment, :comment_question, body: "The slings and arrows of outrageous fortune")}
+  given!(:comment2) { create(:comment, :comment_question, body: 'Or to take arms against a sea of troubles')}
+  given!(:user1) { create(:user, email: "user1@tst.com")}
+  given!(:user2) { create(:user, email: "user2@tst.com")}
   before { visit search_get_search_path }
 
   describe 'Searching in ', js: true, sphinx: true do
@@ -10,11 +16,61 @@ feature 'Guests and users can search a phrase in Questions, Answers, Users and C
       ThinkingSphinx::Test.run do
         within 'form' do
           fill_in 'Search:', with: 'to take arms'
+          check "question_flag"
           click_on 'Search'
         end
         expect(current_path).to eq search_get_search_path
         within '#question' do
           expect(page).to have_content question2.id
+          expect(page).to_not have_content question1.id
+
+        end
+      end
+    end
+
+    scenario 'an answer' do
+      ThinkingSphinx::Test.run do
+        within 'form' do
+          fill_in 'Search:', with: 'to take arms'
+          check "answer_flag"
+          click_on 'Search'
+        end
+        expect(current_path).to eq search_get_search_path
+        within '#answer' do
+          expect(page).to have_content answer2.id
+          expect(page).to_not have_content answer1.id
+
+        end
+      end
+    end
+
+    scenario 'a comment' do
+      ThinkingSphinx::Test.run do
+        within 'form' do
+          fill_in 'Search:', with: 'to take arms'
+          check "comment_flag"
+          click_on 'Search'
+        end
+        expect(current_path).to eq search_get_search_path
+        within '#comment' do
+          expect(page).to have_content comment2.id
+          expect(page).to_not have_content comment1.id
+
+        end
+      end
+    end
+
+    scenario 'a user' do
+      ThinkingSphinx::Test.run do
+        within 'form' do
+          fill_in 'Search:', with: 'user2'
+          check "user_flag"
+          click_on 'Search'
+        end
+        expect(current_path).to eq search_get_search_path
+        within '#user' do
+          expect(page).to have_content user2.id
+          expect(page).to_not have_content user1.id
         end
       end
     end
