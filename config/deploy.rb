5f0d59e70edf776e0172773a67c24cf0d6664c4c
Dmitry <46634431+DmitryKilin@users.noreply.github.com>
2020-01-13 22:58:15 +0300

    add Cap configure files

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.2"
+
+set :application, "qna"
+set :repo_url, "git@github.com:DmitryKilin/qna.git"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/qna"
+set :deploy_user, 'deployer'
+
+# Sidekiq service defaults
+set :init_system, :systemd
+set :service_unit_name, "sidekiq.service"
+
+# Default value for :linked_files is []
+append :linked_files, "config/database.yml", "config/master.key"
+
+# Default value for linked_dirs is []
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
--- config/schedule.rb
+++ config/schedule.rb
@@ -18,6 +18,9 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
+every 1.day do
+  runner "DailyDigestJob.perform_now"
+end
 
 every 30.minutes do
   rake "ts:index"
